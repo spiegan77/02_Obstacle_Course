8ea0b165a2cf1a4492295bf8bc3c664abc1827b3
Steve_U57 <obsidian77@gmail.com>
2021-03-03 18:35:57 -0500

    15_Basic_Collision

---
+++ Assets/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]  float moveSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Placeholder
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(xValue, 0, zValue);
+    }
+}
