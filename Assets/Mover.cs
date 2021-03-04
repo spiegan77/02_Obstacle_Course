954b6e96466c6ace1a97890243bd712e9687ee92
Steve_U57 <obsidian77@gmail.com>
2021-03-04 18:33:05 -0500

    19_Incrementing _A_Score

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -9,15 +9,25 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Placeholder
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-
         transform.Translate(xValue, 0, zValue);
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Be excellent to each other");
     }
 }
---
+++ Assets/Scorer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    int hits = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        hits++;
+        Debug.Log("You've bumped into a thing this many time: " + hits);
+    }
+}
